ef39f907be51588bba76efeb2f76ea525b5b23aa
Levi Titan <levi@Levis-MacBook-Pro.local>
2018-11-01 13:48:20 +0000

    reorg files

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ fn main() {
 	let pkey: [u8; 32] = [1; 32];
 	let bytecode: Vec<stack_types> = vec![PUSH, byte(0x01), PUSH, bytes256(pkey)];
 	let s = VM::new(bytecode);
-	s.execute();
+	s.execute(); 
 }
 
